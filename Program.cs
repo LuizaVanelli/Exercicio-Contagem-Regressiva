08fa4e0b2302fabd92b353f4db252058011638a2
LuizaVanelli <luizavanelli.vanelli@gmail.com>
2021-08-28 22:53:42 -0300

    Atividade de Contagem Regressiva

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercicio_Contagem_Regressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Preparar...");
+            Console.ReadKey();
+            Console.WriteLine("Apontar...");
+            Console.ReadKey();
+            Console.WriteLine("FOGO...");
+            Console.ReadKey();
+        }
+    }
+}
